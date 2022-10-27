ad14c533d9c1f6de1c30864a643949254a29ac15
Ariprasath30 <prasathari30@gmail.com>
2022-10-27 11:47:08 +0530

    My task

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,27 @@
+package org.add;
+public class GreensTech {
+	public void GreensOmr() {
+System.out .println("greens omr ; no.1");	}
+	public void GreensAdayar() {
+		System.out .println("greens adayar ; no.2");	}
+	public void GreensTambaram() {
+		System.out .println("greens tambaram ; no.3");	}
+	public void  GreensVelacherry() {
+		System.out .println("greens velacherry ; no.4");	}
+	public void GreensAnnanagr() {
+		System.out .println("greens annangar ; no.5");	}
+	//main method creation
+	//main ctrl+space and press enter
+	public static void main (String[]args) {
+	//object creation
+		//classname objectname = new classname();
+		GreensTech a = new GreensTech();
+		//method calling 
+		//objectname . methodname();
+		a.GreensOmr();
+		a.GreensAdayar();
+		a.GreensTambaram();
+		a.GreensVelacherry();
+		a.GreensAnnanagr();
+	}
+	}
